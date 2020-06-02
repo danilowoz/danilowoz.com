5dc74c7c8ac7b74001629927f31aaf80ff070360
Danilo Woznica <danilowoz@gmail.com>
2020-06-02 20:38:40 +0100

    feat(post): improves

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+import Highlight, { defaultProps, Language } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/github'
 
 import 'wipe.css'
 import './root.css'
 import './typography.css'
 
-const Layout: React.FC = ({ children }) => <>{children}</>
+const CodeBlock = ({
+  children,
+  className = 'js',
+}: {
+  children: string
+  className: string
+}) => {
+  const language = className.replace(/language-/, '') as Language
+
+  return (
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={children.trim()}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
+
+const components = { code: CodeBlock } as any
+
+const Layout: React.FC = ({ children }) => (
+  <MDXProvider components={components}>{children}</MDXProvider>
+)
 
 export { Layout }
