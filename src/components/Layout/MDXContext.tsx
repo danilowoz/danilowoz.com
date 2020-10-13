b08b1ae6f08cc5a8ce730f9726eb8aa88dea66ff
Danilo Woznica <danilowoz@gmail.com>
2020-10-13 20:56:01 +0100

    feat(post): update code theme

--- src/components/Layout/MDXContext.tsx
+++ src/components/Layout/MDXContext.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 import Highlight, { defaultProps, Language } from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/github'
+import theme from 'prism-react-renderer/themes/oceanicNext'
 import { useAmp } from 'next/amp'
 
 const CodeBlock = ({
