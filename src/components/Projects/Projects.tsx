3c15e62f3f2bf1f4a73ff91cb32e48b104b3a326
Danilo Woznica <danilowoz@gmail.com>
2021-06-11 18:54:38 +0100

    feat(projects): new layout

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,57 +4,141 @@ import styled from 'styled-components'
 import { PostsListProps } from 'service/projects'
 
 const Wrapper = styled.div`
-  @media (min-width: 576px) {
-    margin: 8em 0 6em;
+  margin: 1em 0;
+
+  @media (min-width: 769px) {
+    margin: 6em 0;
+  }
+
+  h2 {
+    padding: 1rem 3rem;
+    margin: 0;
+    font-weight: bold;
+    font-size: 1rem;
+  }
+`
+
+const Section = styled.section`
+  background: rgb(var(--color-foreground));
+  border-radius: 0.3em;
+  padding: 1rem;
+
+  @media (min-width: 769px) {
+    padding: 3rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+  h3,
+  p,
+  small {
+    margin: 0;
+    color: rgb(var(--color-background));
+  }
+
+  small {
+    opacity: 0.8;
+  }
+
+  h3 {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 0.6em;
   }
 `
 
 const Item = styled.a`
-  display: flex;
-  border-top: 1px solid #eee;
-  align-items: center;
-  justify-content: space-between;
+  padding-bottom: 3rem;
 
-  h3 {
-    margin: 0.4em 0;
-    color: var(--color-grey-2);
-  }
+  @media (min-width: 769px) {
+    width: 50%;
+    padding: 2rem 0;
+    position: relative;
 
-  p {
-    max-width: 99%;
-    color: var(--color-grey-1);
-    transition: color 300ms ease;
-  }
-
-  .arrow {
-    white-space: nowrap;
-  }
-
-  &:hover {
-    p {
-      color: rgb(var(--color-foreground));
+    &:nth-child(even) {
+      padding-right: 2rem;
     }
-    img {
-      margin-right: 0.5em;
+
+    &:nth-child(odd) {
+      padding-left: 2rem;
     }
   }
 `
 
+const FeatureWrapper = styled.a`
+  position: relative;
+  border-radius: 0.3em;
+
+  background-size: cover;
+  margin: -1rem;
+  padding: 1rem;
+  margin-bottom: 2rem;
+
+  @media (min-width: 769px) {
+    padding: 0;
+    padding-bottom: 30%;
+
+    margin: -3rem;
+    margin-bottom: -16%;
+
+    > div {
+      width: 42%;
+      padding-left: 3rem;
+      padding-top: 12%;
+    }
+  }
+
+  h3 {
+    font-size: 1.4rem;
+  }
+
+  img {
+    display: none;
+
+    @media (min-width: 769px) {
+      display: block;
+      position: absolute;
+      right: -10%;
+      top: -14%;
+      width: 55%;
+    }
+  }
+`
+
+const FEATURED = 'getcoverify.com'
+
 const Projects: React.FC<{ data: PostsListProps[] }> = ({ data }) => {
+  const featuredItem = data.find((e) => e.title === FEATURED)
+  const rest = data.filter((e) => e.title !== FEATURED)
+
   return (
     <Wrapper>
       <h2>Open-source projects</h2>
-      {data.map((item) => {
-        return (
-          <Item key={item.link} href={item.link} target="_blank">
-            <div>
+
+      <Section>
+        <FeatureWrapper
+          href={featuredItem?.link}
+          style={{ backgroundImage: `url(${featuredItem?.cover})` }}
+        >
+          <div>
+            <img src="/images/project-feature.png" alt="featured" />
+            <small>{featuredItem?.categories?.join(' - ')}</small>
+            <h3>{featuredItem?.title}</h3>
+            <p>{featuredItem?.tagline}</p>
+          </div>
+        </FeatureWrapper>
+
+        {rest.map((item) => {
+          return (
+            <Item key={item.link} href={item.link} target="_blank">
+              <small>{item?.categories?.join(' - ')}</small>
               <h3>{item.title}</h3>
               <p>{item.tagline}</p>
-            </div>
-            <span className="arrow">{` ->`}</span>
-          </Item>
-        )
-      })}
+            </Item>
+          )
+        })}
+      </Section>
     </Wrapper>
   )
 }
