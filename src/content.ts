abbe6bb1aaf5eda476de04f6734a8e16814a176a
Danilo Woznica <danilowoz@gmail.com>
2021-12-26 00:13:02 +0000

    hero

--- src/content.ts
+++ src/content.ts
@@ -1,11 +1,10 @@
 export default {
   seo: {
     site: "Danilo Woznica - Design-driven developer",
-    description:
-      "Design-driven developer focused on crafting products in ReactJS.",
+    description: "Design-driven developer focused on making React products.",
   },
   hero: {
-    title: "Design-driven developer focused on crafting products in ReactJS.",
-    headline: "I write about new technologies and my side-projects",
+    title: "Design-driven developer focused on making React products",
+    headline: "and empowering users through web applications.",
   },
 };
