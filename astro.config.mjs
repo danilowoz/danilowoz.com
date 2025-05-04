9f048a36c3c6f2779448ff45803f72610e1ef3ab
Danilo Woznica <danilowoz@gmail.com>
2025-05-04 13:38:57 +0200

    drop cf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,28 @@
-import { defineConfig } from "astro/config";
-import robotsTxt from "astro-robots-txt";
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://danilowoz",
-  integrations: [
-    react(),
-    robotsTxt({
-      sitemap: false,
-    }),
-  ],
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: "min-light",
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
-  },
-  output: "server",
-  adapter: cloudflare(),
+	site: "https://danilowoz",
+	integrations: [
+		react(),
+		robotsTxt({
+			sitemap: false,
+		}),
+	],
+	markdown: {
+		shikiConfig: {
+			// Choose from Shiki's built-in themes (or add your own)
+			// https://github.com/shikijs/shiki/blob/main/docs/themes.md
+			theme: "min-light",
+			// Add custom languages
+			// Note: Shiki has countless langs built-in, including .astro!
+			// https://github.com/shikijs/shiki/blob/main/docs/languages.md
+			langs: [],
+			// Enable word wrap to prevent horizontal scrolling
+			wrap: true,
+		},
+	},
+	output: "server",
 });
